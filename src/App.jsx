57d5dce0073cc1edd6c38968f291d392230dc8c9
leon <leonkluska0@gmail.com>
2025-11-14 10:41:08 +0100

    add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import img from './assets/img.png'
 
 function App() {
   return (
     <>
         <h1>Pizza net</h1>
+        <img src={img} alt="Pizza" style={{width: '200px'}} />
     </>
   )
 }
