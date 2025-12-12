1055c41cafe5addb24f0d7e6feb221b41da3f5df
leon <leonkluska0@gmail.com>
2025-12-12 19:59:49 +0100

    change urls

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,9 @@ export default defineConfig({
     host: true,
     port: 5173,
     proxy: {
-      // 1. Ruch do autoryzacji kierujemy prosto do auth-service
-      '/api/auth': {
-        target: 'http://auth-service:8081', // Bezpośrednio do kontenera Auth
-        changeOrigin: true,
-        secure: false,
-        // Usuwamy prefiks /api, bo auth-service spodziewa się /auth/...
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-
-      // 2. Ruch do pizzy kierujemy prosto do menu-service
-      '/api/pizza': {
-        target: 'http://menu-service:8081', // Bezpośrednio do kontenera Menu
+      // Wszystkie requesty /api/** idą przez gateway-service
+      '/api': {
+        target: 'http://gateway-service:8080',
         changeOrigin: true,
         secure: false,
       }
